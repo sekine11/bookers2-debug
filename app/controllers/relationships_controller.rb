75c55563a81fea8583e002a79f36c773d6887297
sekine11 <sekine11665@gmail.com>
2020-04-02 23:39:17 +0900

    [Finish] Task4

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,38 @@
+class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    user = User.find(params[:follow_id])
+    following = current_user.follow(user)
+    if following.save
+      flash[:success] = 'ユーザーをフォローしました'
+      redirect_back(fallback_location: :back)
+    else
+      flash.now[:alert] = 'ユーザーのフォローに失敗しました'
+      redirect_back(fallback_location: :back)
+    end
+  end
+
+  def destroy
+    user = User.find(params[:follow_id])
+    following = current_user.unfollow(user)
+    if following.destroy
+      flash[:success] = 'ユーザーのフォローを解除しました'
+      redirect_back(fallback_location: :back)
+    else
+      flash.now[:alert] = 'ユーザーのフォロー解除に失敗しました'
+      redirect_back(fallback_location: :back)
+    end
+  end
+
+  def following_index
+  	@user = User.find(params[:id])
+  	@users = @user.followings
+  end
+
+  def follow_index
+  	@user = User.find(params[:id])
+  	@users = @user.followers
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :users,only: [:show,:index,:edit,:update]
+
+  get "/users/:id/relationships/follow" => "relationships#follow_index", as: "follow"
+  get "/users/:id/relationships/following" => "relationships#following_index", as: "following"
 
   resources :books do
   	resource :favorites, only: [:create, :destroy]
@@ -10,5 +14,7 @@ Rails.application.routes.draw do
 
   root to: "home#top"
   get "home/about" => "home#about"
+
+  resources :relationships, only: [:create, :destroy]
 
 end
